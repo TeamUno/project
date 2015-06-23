47a9f0205926fdea8cc7b1031197c512e175568d
existeundelta <existeundelta@gmail.com>
2015-06-23 17:11:51 +0200

    cache false post submit mongoimport -d wherelocalsgo -c demographic_distribution --type tsv --file demographic_distribution000 --fields zipcode,date,category,ageinterval,gender,merchant,card,payment,avg,max,min,std
    http://api.mongodb.org/python/current/api/pymongo/cursor.html

--- WhereLocalsGo/static/js/services.js
+++ WhereLocalsGo/static/js/services.js
@@ -2,17 +2,16 @@ angular.module('starter.services', [])
 
 .factory('Places', function($http) {
   // Might use a resource here that returns a JSON array
-
-
   return {
-    all: function(callback) {
+    all: function(rootScope,callback) {
+      console.log(JSON.stringify(rootScope.preferences));
       var req = {
         method: 'POST',
         url: '/api/v1/places',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
         },
-        data: 'email='
+        data: rootScope.preferences //'category=' + rootScope.preferences.category +'&age=' + rootScope.preferences.age +'&gender=' + rootScope.preferences.gender +'&amount=' + rootScope.preferences.amount
       };
       $http(req).success(callback);
     },
