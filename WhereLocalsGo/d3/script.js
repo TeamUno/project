a99c449cc3c6a090ba896258ef4051a953873701
existeundelta <existeundelta@gmail.com>
2015-07-01 18:00:33 +0200

    Example how look like for spend in a zipcode by categories

--- WhereLocalsGo/d3/script.js
+++ WhereLocalsGo/d3/script.js
@@ -4,56 +4,22 @@ var w = 500,
 var colorscale = d3.scale.category10();
 
 //Legend titles
-var LegendOptions = ['Smartphone','Tablet'];
+var LegendOptions = ['08172','08002'];
 
 //Data
 var d = [
     [
-        {axis:"Email",value:0.59},
-        {axis:"Social Networks",value:0.56},
-        {axis:"Internet Banking",value:0.42},
-        {axis:"News Sportsites",value:0.34},
-        {axis:"Search Engine",value:0.48},
-        {axis:"View Shopping sites",value:0.14},
-        {axis:"Paying Online",value:0.11},
-        {axis:"Buy Online",value:0.05},
-        {axis:"Stream Music",value:0.07},
-        {axis:"Online Gaming",value:0.12},
-        {axis:"Navigation",value:0.27},
-        {axis:"App connected to TV program",value:0.03},
-        {axis:"Offline Gaming",value:0.12},
-        {axis:"Photo Video",value:0.4},
-        {axis:"Reading",value:0.03},
-        {axis:"Listen Music",value:0.22},
-        {axis:"Watch TV",value:0.03},
-        {axis:"TV Movies Streaming",value:0.03},
-        {axis:"Listen Radio",value:0.07},
-        {axis:"Sending Money",value:0.18},
-        {axis:"Other",value:0.07},
-        {axis:"Use less Once week",value:0.08}
+        {axis:"es_faschion",value:0.59},
+        {axis:"es_food",value:0.56},
+        {axis:"es_barsandrestaurants",value:0.42},
+        {axis:"es_health",value:0.56},
+        {axis:"es_",value:0.42}
     ],[
-        {axis:"Email",value:0.48},
-        {axis:"Social Networks",value:0.41},
-        {axis:"Internet Banking",value:0.27},
-        {axis:"News Sportsites",value:0.28},
-        {axis:"Search Engine",value:0.46},
-        {axis:"View Shopping sites",value:0.29},
-        {axis:"Paying Online",value:0.11},
-        {axis:"Buy Online",value:0.14},
-        {axis:"Stream Music",value:0.05},
-        {axis:"Online Gaming",value:0.19},
-        {axis:"Navigation",value:0.14},
-        {axis:"App connected to TV program",value:0.06},
-        {axis:"Offline Gaming",value:0.24},
-        {axis:"Photo Video",value:0.17},
-        {axis:"Reading",value:0.15},
-        {axis:"Listen Music",value:0.12},
-        {axis:"Watch TV",value:0.1},
-        {axis:"TV Movies Streaming",value:0.14},
-        {axis:"Listen Radio",value:0.06},
-        {axis:"Sending Money",value:0.16},
-        {axis:"Other",value:0.07},
-        {axis:"Use less Once week",value:0.17}
+        {axis:"es_faschion",value:0.48},
+        {axis:"es_food",value:0.41},
+        {axis:"es_barsandrestaurants",value:0.27},
+        {axis:"es_health",value:0.56},
+        {axis:"es_",value:0.42}
     ]
 ];
 
@@ -88,7 +54,7 @@ var text = svg.append("text")
     .attr("y", 10)
     .attr("font-size", "12px")
     .attr("fill", "#404040")
-    .text("What % of owners use a specific service in a week");
+    .text("What % of spend in categories");
 
 //Initiate Legend
 var legend = svg.append("g")
