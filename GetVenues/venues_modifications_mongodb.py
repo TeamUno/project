bc77e49c8acdd7ffe08da955207851510106f38d
Diego Villuendas <villuendas.diego@gmail.com>
2015-07-03 11:12:09 +0200

    Some small changes like adding the number of rates per zipcode and phyton style convention fixes (bullshit :D).

--- GetVenues/venues_modifications_mongodb.py
+++ GetVenues/venues_modifications_mongodb.py
@@ -10,6 +10,7 @@ import re
 import numpy as np
 from operator import itemgetter, attrgetter
 import csv
+import string
 
 # Connection to Mongo DB
 try:
@@ -21,6 +22,13 @@ except pymongo.errors.ConnectionFailure, e:
 db = conn['TeamUno']
 venuesGP=db.venuesGooglePlaces
 venuesFS=db.venuesFoursquare
+
+#for u in venuesFS.find():
+#    if u['merchant_zipcode']!=None:
+#        u['merchant_zipcode']=u['merchant_zipcode'].zfill(5)
+#        if len(u['merchant_zipcode'])>5 or not u['merchant_zipcode'].isdigit():
+#            u['merchant_zipcode']=None    
+#    venuesFS.save(u)
 
 ## ------------------------------------------------------------------
 ## Extract ZIP from formatted address field and adds a new key zipcode:ZIP
@@ -36,15 +44,15 @@ venuesFS=db.venuesFoursquare
 #
 ##Get all categories
 #
-#data=[u['category'] for u in venuesFS.find({},{'category':1})]
+#data = [u['category'] for u in venuesFS.find({}, {'category':1})]
 #
 ##Count them
 #
-#data=dict((x, data.count(x)) for x in data)
+#data = dict((x, data.count(x)) for x in data)
 #
 ##Sort the categories per appearances
 #
-#a=sorted([[key,value] for key,value in data.iteritems()],key=itemgetter(1),reverse=True)
+#a = sorted([[key,value] for key,value in data.iteritems()], key=itemgetter(1), reverse=True)
 #
 #Save to a list to check manually on Excel if the category fits in Bar&Restaurant
 #
@@ -54,7 +62,7 @@ venuesFS=db.venuesFoursquare
 #
 ## Fits in or not    
 #
-#catfilt=[1,1,1,1,1,0,1,0,0,1,0,1,1,0,1,1,1,0,1,1,1,1,1,1,1,0,1,0,0,1,0,1,0,0,0,1,0,0,1,0,0,1,1,1,1,1,0,0,0,1,0,0,0,0,0,0,1,0,0,1,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,1,0,1,0,0,1,0,0,1,1,1,0,1,1,0,0,0,0,1,0,1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,1,0,0,0,0,0,0,1,1,1,0,1,0,1,0,0,0,0,1,1,1,0,1,0,0,0,0,1,1,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,1,1,0,0,0,1,0,1]
+#catfilt = [1,1,1,1,1,0,1,0,0,1,0,1,1,0,1,1,1,0,1,1,1,1,1,1,1,0,1,0,0,1,0,1,0,0,0,1,0,0,1,0,0,1,1,1,1,1,0,0,0,1,0,0,0,0,0,0,1,0,0,1,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,1,0,1,0,0,1,0,0,1,1,1,0,1,1,0,0,0,0,1,0,1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,1,0,0,0,0,0,0,1,1,1,0,1,0,1,0,0,0,0,1,1,1,0,1,0,0,0,0,1,1,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,1,1,0,0,0,1,0,1]
 #b=[]
 #for i in range(0,len(a)):
 #    if catfilt[i]==1:
@@ -62,29 +70,30 @@ venuesFS=db.venuesFoursquare
 #
 ##Categories in the dataset that fit
 #
-#b=[u'Mediterranean Restaurant', u'Restaurant', u'Caf\xe9', u'Spanish Restaurant', u'Tapas Restaurant', u'Bar', u'Pizza Place', u'Italian Restaurant', u'Coffee Shop', u'Fast Food Restaurant', u'Cocktail Bar', u'Seafood Restaurant', u'Nightclub', u'Burger Joint', u'Ice Cream Shop', u'Sandwich Place', u'Diner', u'Breakfast Spot', u'Food', u'Japanese Restaurant', u'Pub', u'Sporting Goods Shop', u'Paella Restaurant', u'Snack Place', u'BBQ Joint', u'Gastropub', u'Chinese Restaurant', u'Brewery', u'American Restaurant', u'Asian Restaurant', u'Sushi Restaurant', u'Argentinian Restaurant', u'Steakhouse', u'Hot Dog Joint', u'Mexican Restaurant', u'Beach Bar', u'Wine Bar', u'French Restaurant', u'Vegetarian / Vegan Restaurant', u'Ramen / Noodle House', u'Fried Chicken Joint', u'Falafel Restaurant', u'Hotel Bar', u'Turkish Restaurant', u'Tea Room', u'Food Court', u'Cafeteria', u'Bistro', u'Indian Restaurant', u'Kebab Restaurant', u'Caribbean Restaurant', u'Food Truck', u'Frozen Yogurt', u'Thai Restaurant', u'Comedy Club', u'Gay Bar', u'Molecular Gastronomy Restaurant', u'Dive Bar', u'Creperie', u'Hookah Bar', u'Middle Eastern Restaurant', u'German Restaurant', u'Gluten-free Restaurant', u'Juice Bar', u'Pakistani Restaurant', u'Korean Restaurant', u'Salad Place', u'New American Restaurant', u'Peruvian Restaurant', u'Home Cooking Restaurant', u'Buffet', u'Karaoke Bar', u'Greek Restaurant', u'Burrito Place', u'English Restaurant', u'Latin American Restaurant', u'Whisky Bar', u'Irish Pub', u'Vietnamese Restaurant', u'Taco Place', u'Piano Bar']
+#b = [u'Mediterranean Restaurant', u'Restaurant', u'Caf\xe9', u'Spanish Restaurant', u'Tapas Restaurant', u'Bar', u'Pizza Place', u'Italian Restaurant', u'Coffee Shop', u'Fast Food Restaurant', u'Cocktail Bar', u'Seafood Restaurant', u'Nightclub', u'Burger Joint', u'Ice Cream Shop', u'Sandwich Place', u'Diner', u'Breakfast Spot', u'Food', u'Japanese Restaurant', u'Pub', u'Sporting Goods Shop', u'Paella Restaurant', u'Snack Place', u'BBQ Joint', u'Gastropub', u'Chinese Restaurant', u'Brewery', u'American Restaurant', u'Asian Restaurant', u'Sushi Restaurant', u'Argentinian Restaurant', u'Steakhouse', u'Hot Dog Joint', u'Mexican Restaurant', u'Beach Bar', u'Wine Bar', u'French Restaurant', u'Vegetarian / Vegan Restaurant', u'Ramen / Noodle House', u'Fried Chicken Joint', u'Falafel Restaurant', u'Hotel Bar', u'Turkish Restaurant', u'Tea Room', u'Food Court', u'Cafeteria', u'Bistro', u'Indian Restaurant', u'Kebab Restaurant', u'Caribbean Restaurant', u'Food Truck', u'Frozen Yogurt', u'Thai Restaurant', u'Comedy Club', u'Gay Bar', u'Molecular Gastronomy Restaurant', u'Dive Bar', u'Creperie', u'Hookah Bar', u'Middle Eastern Restaurant', u'German Restaurant', u'Gluten-free Restaurant', u'Juice Bar', u'Pakistani Restaurant', u'Korean Restaurant', u'Salad Place', u'New American Restaurant', u'Peruvian Restaurant', u'Home Cooking Restaurant', u'Buffet', u'Karaoke Bar', u'Greek Restaurant', u'Burrito Place', u'English Restaurant', u'Latin American Restaurant', u'Whisky Bar', u'Irish Pub', u'Vietnamese Restaurant', u'Taco Place', u'Piano Bar']
 #
-#dataCAT=[u for u in venuesFS.find({'category': {'$in': b}})]
+#dataCAT = [u for u in venuesFS.find({'category': {'$in': b}})]
 #for u in venuesFS.find({'category': {'$in': b}}):
-#    u['supercategory']='bars_and_restaurants'
+#    u['supercategory'] = 'bars_and_restaurants'
 #    venuesFS.save(u)
 #for u in venuesFS.find({'category': {'$nin': b}}):
-#    u['supercategory']='others'
+#    u['supercategory'] = 'others'
 #    venuesFS.save(u)
 
 ## ------------------------------------------------------------------
 ## Get only the venues with zipcode, rating and price, and normalize price and rating
 ## ------------------------------------------------------------------
 #
-db.venuesFS_filter.insert_many([u for u in venuesFS.find({'$and':[{'rating': {'$ne': None}},{'category':{'$in':b}},{'price': {'$ne': None}}]})])
-db.venuesGP_filter.insert_many([u for u in venuesGP.find({'$and':[{'rating': {'$exists': 'true'}},{'formatted_address':{'$regex': '\d{5}'}},{'types':{'$nin':['store']}},{'price_level':{'$exists':'true'}}]})])
+#db.venuesFS_filter.insert_many([u for u in venuesFS.find({'$and':[{'rating': {'$ne': None}}, {'category':{'$in':b}}, {'price': {'$ne': None}},{'merchant_zipcode':{'$regex': '\d{5}'}}]})])
 
-for u in db.venuesFS_filter.find():
-    u['price_norm']=(u['price']-1)/3.
-    u['rating_norm']=u['rating']/10.
-    db.venuesFS_filter.save(u)
- 
-for u in db.venuesGP_filter.find():
-    u['price_norm']=u['price_level']/4.
-    u['rating_norm']=(u['rating']-1)/4.
+#db.venuesGP_filter.insert_many([u for u in venuesGP.find({'$and':[{'rating': {'$exists': 'true'}}, {'formatted_address':{'$regex': '\d{5}'}},{'types':{'$nin':['store']}}, {'price_level':{'$exists':'true'}}]})])
+#
+#for u in db.venuesFS_filter.find():
+#    u['price_norm'] = (u['price']-1)/3.
+#    u['rating_norm'] = u['rating']/10.
+#    db.venuesFS_filter.save(u)
+# 
+#for u in db.venuesGP_filter.find():
+#    u['price_norm']=u['price_level']/4.
+#    u['rating_norm']=(u['rating']-1)/4.
 #    db.venuesGP_filter.save(u)
