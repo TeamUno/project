2f430b64247234c7f7452aa2d2f7919c22672aea
existeundelta <existeundelta@gmail.com>
2015-07-04 14:21:30 +0200

    app default to map

--- WhereLocalsGo/static/js/app.js
+++ WhereLocalsGo/static/js/app.js
@@ -81,6 +81,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/map');
 
 });
