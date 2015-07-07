f8e023a778c6e32ee778dc40fed3ed97b8b2c8d9
daviz <daviz@Davids-MacBook-Pro.local>
2015-07-07 20:12:30 +0200

    moving files

--- WhereLocalsGo/probability-model.py
+++ WhereLocalsGo/probability-model.py
@@ -22,7 +22,7 @@ db.drop_collection("merchant_zipcode_coordinates")
 
 # Load datasets
 names = ["merchant_zipcode", "date", "category", "age_interval", "gender", "merchants", "cards", "payments", "avg_payment", "max_payment", "min_payment", "std"]
-demo_stats = pd.read_csv("dataset/demographic_distribution000", delim_whitespace=True, names= names, parse_dates=["date"], dtype = {'merchant_zipcode': str})
+demo_stats = pd.read_csv("../dataset/demographic_distribution000", delim_whitespace=True, names= names, parse_dates=["date"], dtype = {'merchant_zipcode': str})
 
 bcn_zipcodes = ['08001', '08002', '08003', '08004', '08005', '08006', '08007',
                 '08008', '08009', '08010', '08011', '08012', '08013', '08014',
@@ -47,7 +47,7 @@ gbcn = gbcn.reset_index()
 total_by_zipcode = gbcn.groupby(["gender" , "age_interval", "weekday"])["payments"].max()
 gbcn['payments_proportion'] = gbcn.apply(lambda row: np.true_divide(row.payments, total_by_zipcode[row.gender, row.age_interval, row.weekday]), axis=1)
 
-zip_code_geojson = pygeoj.load(filepath="dataset/best_bcn.geojson")
+zip_code_geojson = pygeoj.load(filepath="../dataset/best_bcn.geojson")
 
 geojson = {
     "type": "FeatureCollection",
