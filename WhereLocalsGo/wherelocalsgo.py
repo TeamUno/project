7529760e500b974543d596ab75be1031d4b1547f
daviz <daviz@Davids-MacBook-Pro.local>
2015-07-10 12:57:22 +0200

    applying naive bayes on the diferent probabilities for gender, age_interval, weekday, customerzipcode

--- WhereLocalsGo/wherelocalsgo.py
+++ WhereLocalsGo/wherelocalsgo.py
@@ -20,6 +20,13 @@ age_ranges = { "<25":   range(0, 25),
                "45-54": range(45, 55),
                "55-65": range(55, 65),
                ">65":   range(65, 100) }
+bcn_zipcodes = ['08001', '08002', '08003', '08004', '08005', '08006', '08007',
+                '08008', '08009', '08010', '08011', '08012', '08013', '08014',
+                '08015', '08016', '08017', '08018', '08019', '08020', '08021',
+                '08022', '08023', '08024', '08025', '08026', '08027', '08028',
+                '08029', '08030', '08031', '08032', '08033', '08034', '08035',
+                '08036', '08037', '08038', '08039', '08040', '08041', '08042']
+
 
 class IndexHandler(web.RequestHandler):
     def get(self):
@@ -52,10 +59,14 @@ class MapsHandler(web.RequestHandler):
         preference_data = {}
         gender = ""
         age_interval = ""
+        customerzipcode = ""
+
         if self.request.body != "":
             preference_data = json.loads(self.request.body)
         if preference_data["gender"] != '':
             gender = preference_data["gender"]
+        if preference_data["customerzipcode"] != '':
+            customerzipcode = preference_data["customerzipcode"]
         if preference_data["age"] != '':
             for key, ranges in age_ranges.iteritems():
                 if int(preference_data["age"]) in ranges:
@@ -65,26 +76,71 @@ class MapsHandler(web.RequestHandler):
         else:
             weekday = str(datetime.date.today().weekday())
 
-        if preference_data["gender"] != '':
-            query={ "age_interval": age_interval,
-                          "gender": gender,
-                          "weekday": weekday }
-            order =pymongo.DESCENDING
-        else:
-            query={ "age_interval": age_interval,
-                          "weekday": weekday }
-            order = pymongo.ASCENDING
+# TODO remove?
+#        if preference_data["gender"] != '':
+#            query={ "age_interval": age_interval,
+#                          "gender": gender,
+#                          "weekday": weekday }
+# #           order =pymongo.DESCENDING
+#        else:
+#            query={ "age_interval": age_interval,
+#                          "weekday": weekday }
+##            order = pymongo.ASCENDING
+#        zipcode_aggregation = db.merchant_zipcode_aggregation.find(query).sort("payments_proportion", order)
 
-        zipcode_aggregation = db.merchant_zipcode_aggregation.find(query).sort("payments_proportion", order)
 
-        geo_json = db.merchant_zipcode_coordinates.find({}, {"_id": 0}).next()
-        geo_json_aux = geo_json
-        for aggregation in zipcode_aggregation:
-            index = next(index  for (index, feature) in enumerate(geo_json['features']) if feature['properties']['zipcode'] == aggregation['merchant_zipcode'])
-            geo_json_aux['features'][index]["properties"]["payments_proportion"] = aggregation['payments_proportion']
+
+        # We calculate probability where a user will go to a merchant_zipcode based on the preferences using
+        # Naive Bayes calculations.
+        zipcode_proba = naive_bayes_probabilities(age_interval, gender, weekday, customerzipcode)
+
+        # We normalize values relative to the max probability found, so we can have a scale from 0-1 where 1 is
+        # the max probability and the following values will be proportion from the max value.
+        zipcode_proba = normalize_relative_to_max(zipcode_proba)
+
+        #interpolate probabilities inside the geo_json properties so the map can find this normalize value for every zipcode
+        geo_json = interpolate_proba_to_geojson_zipcodes(zipcode_proba)
 
         self.set_header("Content-Type", "application/json")
-        self.write(geo_json_aux)
+        self.write(geo_json)
+
+
+def naive_bayes_probabilities(age_interval, gender, weekday, customerzipcode):
+    zipcode_proba = {}
+    for zipcode in bcn_zipcodes:
+        ageinterval_cursor = db.ageinterval_aggregation.find_one( {"age_interval": age_interval, "merchant_zipcode": zipcode})
+        ageinterval_proba = ageinterval_cursor["payments_proportion"] if ageinterval_cursor != None else 0.0000001
+
+        if gender != '':
+            gender_cursor = db.gender_aggregation.find_one( {"gender": gender, "merchant_zipcode": zipcode})
+            gender_proba = gender_cursor["payments_proportion"] if gender_cursor != None else 0.0000001
+        else:
+            gender_proba = 1
+        weekday_cursor = db.weekdayaggregation.find_one( {"weekday": weekday, "merchant_zipcode": zipcode})
+        weekday_proba = weekday_cursor["payments_proportion"] if weekday_cursor != None else 0.0000001
+
+        if customerzipcode != '':
+            customer_cursor = db.costumerzipcode_aggregation.find_one( {"customerzipcode": customerzipcode, "merchant_zipcode": zipcode})
+            customer_proba = customer_cursor["payments_proportion"] if customer_cursor != None else 0.0000001
+        else:
+            customer_proba = 1
+
+        # Naive Bayes probability.
+        zipcode_proba[zipcode] = customer_proba * ageinterval_proba * gender_proba * weekday_proba
+    return zipcode_proba
+
+def normalize_relative_to_max(zipcode_proba):
+    max_proba = zipcode_proba[max(zipcode_proba, key=zipcode_proba.get)]
+    for zipcode in zipcode_proba:
+        zipcode_proba[zipcode] = zipcode_proba[zipcode] / max_proba
+    return zipcode_proba
+
+def interpolate_proba_to_geojson_zipcodes(zipcode_proba):
+    geo_json = db.merchant_zipcode_coordinates.find({}, {"_id": 0}).next()
+    for zipcode in bcn_zipcodes:
+        index = next(index  for (index, feature) in enumerate(geo_json['features']) if feature['properties']['zipcode'] == zipcode)
+        geo_json['features'][index]["properties"]["payments_proportion"] = zipcode_proba[zipcode]
+    return geo_json
 
 settings = {
     "template_path": os.path.join(os.path.dirname(__file__), "templates"),
